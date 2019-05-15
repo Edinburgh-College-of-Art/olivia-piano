8a486ce5f1095732b5469a56fc97519183bdb2f8
mhamilt <matt.hamilton@ed.ac.uk>
2019-05-15 11:08:39 +0100

    add outputs

--- olivia-piano/olivia-piano.ino
+++ olivia-piano/olivia-piano.ino
@@ -8,7 +8,7 @@
 // Buttons
 const uint8_t buttonCount = 2;
 const uint8_t yellowButton = 2;
-const uint8_t blueButton = 3;
+const uint8_t blueButton = 4;
 const uint8_t buttons[buttonCount] = {yellowButton, blueButton};
 //------------------------------------------------------
 // Pots
@@ -22,29 +22,47 @@ const uint8_t pots[potCount] = {leftPot, rightPot, slidePot, pressurePot};
 Bounce debouncer[buttonCount];
 //------------------------------------------------------
 // Output Pins
-const uint8_t solenoids[] = {4, 5, 6, 7, 8};
+const uint8_t solenoidCount = 1;
+const uint8_t solenoid[] = {3, 5, 6, 7, 8};
+const uint8_t motorCount = 1;
+const uint8_t dcMotor[] = {3, 5, 6, 9, 10, 11};
 const uint8_t rackPinion  = 9;
 //------------------------------------------------------
-// Data
+// Readings
 bool buttonValue[buttonCount] = {false};
 unsigned int potValue[buttonCount] = {0};
 //------------------------------------------------------
 void setup()
 {
   Serial.begin(9600);
+  // input setup
   for (uint8_t i  = 0; i < buttonCount; ++i)
   {
     pinMode(buttons[i], INPUT_PULLUP);
     debouncer[i].attach(buttons[i]);
     debouncer[i].interval(5); // interval in ms
   }
+
+  // output setup
+  for (uint8_t i  = 0; i < motorCount; ++i)
+  {
+    pinMode(dcMotor[i], OUTPUT);
+  }
 }
 //------------------------------------------------------
 void loop()
 {
   getInputValues();
-  printInputValues
-  
-  // what do the buttons do
+  printInputValues();
+  //----------------------------------------------------
+  // what do the controls do?
+
+  for (uint8_t i  = 0; i < motorCount; ++i)
+  {
+    uint8_t motorSpeed = map(potValue[0], 0, 1024, 0, 255);
+    analogWrite(dcMotor[i], motorSpeed);
+  }
+  //----------------------------------------------------
+
 }
 //------------------------------------------------------
